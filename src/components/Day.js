112b671a5b75148eed329cc03c91173992f8c37c
Leandro Aranguez <leandro.aranguez@synapse.com>
2019-01-24 09:44:48 -0300

    checkout branch

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,47 +1,45 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 
-class Day extends PureComponent { 
+const Day = props => { 
 
-    render(){
+    const { tasks, engDay, day, today  } = props
 
-        let undoneTasks = this.props.tasks.filter( task => 
-            task.day === this.props.engDay && !task.done );
-        let doneTasks = this.props.tasks.filter(task =>
-            task.day === this.props.engDay && task.done)
+    //pasar esta logica al state
+    let undoneTasks = tasks.filter( task => task.day === engDay && !task.done );
+    let doneTasks = tasks.filter(task => task.day === engDay && task.done)
 
-        let undoneHeight = undoneTasks.length * 12;
-        let doneHeight = doneTasks.length * 12;
+    let undoneHeight = undoneTasks.length * 12;
+    let doneHeight   = doneTasks.length * 12;
 
-        return (
-            <div className={`box`} id={this.props.day}>
-                <div className={`day-box ${ this.props.today ? 'is-today' : '' /*|| props.over ? 'day-over' : ''*/ }`}
-                        onClick={ () => this.props.onHandleModal(this.props.engDay, false) }> {/* evitar crear funciones en render*/}
-                    <span className='day'>
-                        {this.props.day.substr(0,3)}
-                        {this.props.today && <span className="is-today">today</span>}
-                    </span>
-    
-                    <span className="max-tasks"
-                            style={{'color': `${undoneTasks.length >= 7 ? '#FF3A69' : '#ccc'}`}}>
-                            max</span>
-                    <span className="undone-tasks"
-                            style={{'color': `${undoneTasks.length === 0 ? '#ccc' : '#FF3A69'}`}}>
-                            {undoneTasks.length}</span>
-                    <span className="done-tasks"
-                            style={{'color': `${doneTasks.length === 0 ? '#ccc' : '#00cf6f'}`}}>
-                            {doneTasks.length}</span>
-                </div>
-    
-                { undoneTasks.length > 0 && (
-                    <span className="task undone"
-                            style={{'height': undoneHeight + '%'}}></span>) }
-    
-                { doneTasks.length > 0 && (
-                    <span className="task done"
-                        style={{'height': doneHeight + '%'}}></span>) }
+    return (
+        <div className="box" id={day}>
+            <div className={`day-box ${ today ? 'is-today' : '' /*|| over ? 'day-over' : ''*/ }`}
+                    onClick={ () => props.onHandleModal(engDay, false) }>
+                <span className='day'>
+                    {day.substr(0,3)}
+                    {today && <span className="is-today">today</span>}
+                </span>
+
+                <span className="max-tasks"
+                      style={{'color': `${undoneTasks.length >= 7 ? '#FF3A69' : '#ccc'}`}}>
+                      max</span>
+                <span className="undone-tasks"
+                      style={{'color': `${undoneTasks.length === 0 ? '#ccc' : '#FF3A69'}`}}>
+                      {undoneTasks.length}</span>
+                <span className="done-tasks"
+                      style={{'color': `${doneTasks.length === 0 ? '#ccc' : '#00cf6f'}`}}>
+                      {doneTasks.length}</span>
             </div>
-        )
-    }
+
+            { undoneTasks.length > 0 && (
+                <span className="task undone"
+                      style={{'height': undoneHeight + '%'}}></span>) }
+
+            { doneTasks.length > 0 && (
+                <span className="task done"
+                      style={{'height': doneHeight + '%'}}></span>) }
+        </div>
+    )
 }
 
 export default Day
